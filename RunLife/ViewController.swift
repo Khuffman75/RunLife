1886bfce426489ba7cd56bdedcb3fa198367d58b
Khuffman75 <Khuffman0905@gmail.com>
2021-04-22 09:20:58 -0700

    Safety home page

---
+++ RunLife/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RunLife
+//
+//  Created by Ling, Junmin on 3/8/21.
+//  Copyright © 2021 Hao, Lillian. All rights reserved.
+//
+
+import UIKit
+// test change for git commit - Lillian
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
